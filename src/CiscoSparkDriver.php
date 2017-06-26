b68bb36cc88c86288774f3350c5cfc84ad5c0bae
Marcel Pociot <m.pociot@gmail.com>
2017-06-26 17:13:07 +0200

    Initial commit

---
+++ src/CiscoSparkDriver.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace BotMan\Drivers\CiscoSpark;
+
+use BotMan\BotMan\Drivers\HttpDriver;
+use BotMan\BotMan\Messages\Attachments\Location;
+use BotMan\BotMan\Messages\Incoming\Answer;
+use BotMan\BotMan\Messages\Incoming\IncomingMessage;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use BotMan\BotMan\Users\User;
+use Illuminate\Support\Collection;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CiscoSparkDriver extends HttpDriver
+{
+    const DRIVER_NAME = 'CiscoSpark';
+
+    const API_ENDPOINT = 'https://api.ciscospark.com/v1/';
+
+    /** @var string|null */
+    private $botId;
+
+    /**
+     * @param Request $request
+     */
+    public function buildPayload(Request $request)
+    {
+        $this->payload = new ParameterBag((array) json_decode($request->getContent(), true));
+        $this->event = Collection::make($this->payload->get('data'));
+        $this->config = Collection::make($this->config->get('cisco-spark'));
+    }
+
+    /**
+     * @return array
+     */
+    protected function getHeaders()
+    {
+        return [
+            'Accept:application/json',
+            'Content-Type:application/json',
+            'Authorization:Bearer '.$this->config->get('token')
+        ];
+    }
+
+    /**
+     * Determine if the request is for this driver.
+     *
+     * @return bool
+     */
+    public function matchesRequest()
+    {
+        return ! is_null($this->payload->get('actorId')) && $this->payload->get('resource') === 'messages'  && $this->payload->get('event') === 'created';
+    }
+
+    /**
+     * @param  \BotMan\BotMan\Messages\Incoming\IncomingMessage $message
+     * @return Answer
+     */
+    public function getConversationAnswer(IncomingMessage $message)
+    {
+        return Answer::create($message->getText())->setMessage($message);
+    }
+
+    /**
+     * Retrieve the chat message.
+     *
+     * @return array
+     */
+    public function getMessages()
+    {
+        $this->getBotId();
+
+        $messageContent = $this->getMessageContent($this->event->get('id'));
+
+        $message = new IncomingMessage($messageContent->text, $messageContent->roomId, $messageContent->personId, $messageContent);
+
+        if ($this->getBotId() === $messageContent->personId) {
+            $message->setIsFromBot(true);
+        }
+
+        return [$message];
+    }
+
+    /**
+     * @param $messageId
+     * @return mixed
+     */
+    protected function getMessageContent($messageId)
+    {
+        $response = $this->http->get(self::API_ENDPOINT.'messages/'.$messageId, [], $this->getHeaders());
+
+        return json_decode($response->getContent());
+    }
+
+    /**
+     * @param string|Question|IncomingMessage $message
+     * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $matchingMessage
+     * @param array $additionalParameters
+     * @return Response|null
+     */
+    public function buildServicePayload($message, $matchingMessage, $additionalParameters = [])
+    {
+        $parameters = array_merge_recursive([
+            'roomId' => $matchingMessage->getSender(),
+        ], $additionalParameters);
+        /*
+         * If we send a Question with buttons, ignore
+         * the text and append the question.
+         */
+        if ($message instanceof Question) {
+            $parameters['text'] = $message->getText();
+            $parameters['markdown'] = $message->getText();
+        } elseif ($message instanceof OutgoingMessage) {
+            $parameters['text'] = $message->getText();
+            $parameters['markdown'] = $message->getText();
+
+            $attachment = $message->getAttachment();
+            if (! is_null($attachment) && ! $attachment instanceof Location) {
+                $parameters['files'] = $attachment->getUrl();
+            }
+        } else {
+            $parameters['text'] = $message;
+            $parameters['markdown'] = $message;
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * @param mixed $payload
+     * @return Response
+     */
+    public function sendPayload($payload)
+    {
+        return $this->http->post(self::API_ENDPOINT.'messages', [], $payload, $this->getHeaders(), true);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isConfigured()
+    {
+        return ! is_null($this->config->get('token'));
+    }
+
+    /**
+     * Retrieve User information.
+     * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $matchingMessage
+     * @return User
+     */
+    public function getUser(IncomingMessage $matchingMessage)
+    {
+        $personId = $matchingMessage->getPayload()->personId;
+        $response = $this->http->get(self::API_ENDPOINT.'people/'.$personId, [], $this->getHeaders());
+        $userInfo = Collection::make(json_decode($response->getContent(), true));
+
+        return new User($userInfo->get('id'), $userInfo->get('firstName'), $userInfo->get('lastName'), $userInfo->get('nickName'));
+    }
+
+    /**
+     * Low-level method to perform driver specific API requests.
+     *
+     * @param string $endpoint
+     * @param array $parameters
+     * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $matchingMessage
+     * @return void
+     */
+    public function sendRequest($endpoint, array $parameters, IncomingMessage $matchingMessage)
+    {
+        //
+    }
+
+    /**
+     * Returns the chatbot ID
+     * @return string
+     */
+    private function getBotId()
+    {
+        if (is_null($this->botId)) {
+            $response = $this->http->get(self::API_ENDPOINT.'people/me', [], $this->getHeaders());
+            $bot = json_decode($response->getContent());
+            $this->botId = $bot->id;
+        }
+
+        return $this->botId;
+    }
+}
---
+++ tests/CiscoSparkDriverTest.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace Tests;
+
+use BotMan\BotMan\Http\Curl;
+use BotMan\Drivers\CiscoSpark\CiscoSparkDriver;
+use Mockery as m;
+use PHPUnit_Framework_TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CiscoSparkDriverTest extends PHPUnit_Framework_TestCase
+{
+    private function getDriver($responseData, $htmlInterface = null)
+    {
+        $request = Request::create('', 'POST', [], [], [], [], json_encode($responseData));
+        if ($htmlInterface === null) {
+            $htmlInterface = m::mock(Curl::class);
+        }
+
+        return new CiscoSparkDriver($request, [
+            'cisco-spark' => [
+                'token' => 'my-token'
+            ]
+        ], $htmlInterface);
+    }
+
+    private function getValidTestData()
+    {
+        return [
+            'id' => 'Y2lzY29zcGFyazovL3VzL1dFQkhPT0svZjRlNjA1NjAtNjYwMi00ZmIwLWEyNWEtOTQ5ODgxNjA5NDk3',
+            'name' => 'Guild Chat to http://requestb.in/1jw0w3x1',
+            'resource' => 'messages',
+            'event' => 'created',
+            'filter' => 'roomId=Y2lzY29zcGFyazovL3VzL1JPT00vY2RlMWRkNDAtMmYwZC0xMWU1LWJhOWMtN2I2NTU2ZDIyMDdi',
+            'orgId' => 'Y2lzY29zcGFyazovL3VzL09SR0FOSVpBVElPTi8xZWI2NWZkZi05NjQzLTQxN2YtOTk3NC1hZDcyY2FlMGUxMGY',
+            'createdBy' => 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS8xZjdkZTVjYi04NTYxLTQ2NzEtYmMwMy1iYzk3NDMxNDQ0MmQ',
+            'appId' => 'Y2lzY29zcGFyazovL3VzL0FQUExJQ0FUSU9OL0MyNzljYjMwYzAyOTE4MGJiNGJkYWViYjA2MWI3OTY1Y2RhMzliNjAyOTdjODUwM2YyNjZhYmY2NmM5OTllYzFm',
+            'ownedBy' => 'creator',
+            'status' => 'active',
+            'actorId' => 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS8xZjdkZTVjYi04NTYxLTQ2NzEtYmMwMy1iYzk3NDMxNDQ0MmQ',
+            'data' => [
+                'id' => 'Y2lzY29zcGFyazovL3VzL01FU1NBR0UvMzIzZWUyZjAtOWFhZC0xMWU1LTg1YmYtMWRhZjhkNDJlZjlj',
+                'roomId' => 'Y2lzY29zcGFyazovL3VzL1JPT00vY2RlMWRkNDAtMmYwZC0xMWU1LWJhOWMtN2I2NTU2ZDIyMDdi',
+                'personId' => 'Y2lzY29zcGFyazovL3VzL1BFT1BMRS9lM2EyNjA4OC1hNmRiLTQxZjgtOTliMC1hNTEyMzkyYzAwOTg',
+                'personEmail' => 'person@example.com',
+                'created' => '2015-12-04T17:33:56.767Z'
+            ]
+        ];
+    }
+
+    /** @test */
+    public function it_returns_the_driver_name()
+    {
+        $driver = $this->getDriver([]);
+        $this->assertSame('CiscoSpark', $driver->getName());
+    }
+
+    /** @test */
+    public function it_matches_the_request()
+    {
+        $driver = $this->getDriver([
+            'to' => '41766013098',
+            'messageId' => '0C000000075069C7',
+            'text' => 'Hi Julia',
+            'type' => 'text',
+            'keyword' => 'HEY',
+            'message_timestamp' => '2016-11-30 19:27:46',
+        ]);
+        $this->assertFalse($driver->matchesRequest());
+
+        $driver = $this->getDriver($this->getValidTestData());
+        $this->assertTrue($driver->matchesRequest());
+    }
+
+    /** @test */
+    public function it_returns_the_message_object()
+    {
+        $botResponseData = [
+            'id' => 'bot-id'
+        ];
+        $botResponse = new Response(json_encode($botResponseData));
+
+        $htmlInterface = m::mock(Curl::class);
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/people/me', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($botResponse);
+
+        $msgResponseData = [
+            'text' => 'Hi Julia',
+            'roomId' => 'room-1234567890',
+            'personId' => 'person-0987654321'
+        ];
+        $msgResponse = new Response(json_encode($msgResponseData));
+
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/messages/Y2lzY29zcGFyazovL3VzL01FU1NBR0UvMzIzZWUyZjAtOWFhZC0xMWU1LTg1YmYtMWRhZjhkNDJlZjlj', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($msgResponse);
+
+        $driver = $this->getDriver($this->getValidTestData(), $htmlInterface);
+        $this->assertTrue(is_array($driver->getMessages()));
+    }
+
+    /** @test */
+    public function it_returns_the_message_text()
+    {
+        $botResponseData = [
+            'id' => 'bot-id'
+        ];
+        $botResponse = new Response(json_encode($botResponseData));
+
+        $htmlInterface = m::mock(Curl::class);
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/people/me', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($botResponse);
+
+        $msgResponseData = [
+            'text' => 'Hi Julia',
+            'roomId' => 'room-1234567890',
+            'personId' => 'person-0987654321'
+        ];
+        $msgResponse = new Response(json_encode($msgResponseData));
+
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/messages/Y2lzY29zcGFyazovL3VzL01FU1NBR0UvMzIzZWUyZjAtOWFhZC0xMWU1LTg1YmYtMWRhZjhkNDJlZjlj', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($msgResponse);
+
+        $driver = $this->getDriver($this->getValidTestData(), $htmlInterface);
+        $this->assertSame('Hi Julia', $driver->getMessages()[0]->getText());
+    }
+
+    /** @test */
+    public function it_detects_bots()
+    {
+        $botResponseData = [
+            'id' => 'bot-id'
+        ];
+        $botResponse = new Response(json_encode($botResponseData));
+
+        $htmlInterface = m::mock(Curl::class);
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/people/me', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($botResponse);
+
+        $msgResponseData = [
+            'text' => 'Hi Julia',
+            'roomId' => 'room-1234567890',
+            'personId' => 'bot-id'
+        ];
+        $msgResponse = new Response(json_encode($msgResponseData));
+
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/messages/Y2lzY29zcGFyazovL3VzL01FU1NBR0UvMzIzZWUyZjAtOWFhZC0xMWU1LTg1YmYtMWRhZjhkNDJlZjlj', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($msgResponse);
+
+        $driver = $this->getDriver($this->getValidTestData(), $htmlInterface);
+        $this->assertTrue($driver->getMessages()[0]->isFromBot());
+    }
+
+    /** @test */
+    public function it_returns_the_user_id()
+    {
+        $botResponseData = [
+            'id' => 'bot-id'
+        ];
+        $botResponse = new Response(json_encode($botResponseData));
+
+        $htmlInterface = m::mock(Curl::class);
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/people/me', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($botResponse);
+
+        $msgResponseData = [
+            'text' => 'Hi Julia',
+            'roomId' => 'room-1234567890',
+            'personId' => 'person-0987654321'
+        ];
+        $msgResponse = new Response(json_encode($msgResponseData));
+
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/messages/Y2lzY29zcGFyazovL3VzL01FU1NBR0UvMzIzZWUyZjAtOWFhZC0xMWU1LTg1YmYtMWRhZjhkNDJlZjlj', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($msgResponse);
+
+        $driver = $this->getDriver($this->getValidTestData(), $htmlInterface);
+        $this->assertSame('room-1234567890', $driver->getMessages()[0]->getSender());
+    }
+
+    /** @test */
+    public function it_returns_the_channel_id()
+    {
+        $botResponseData = [
+            'id' => 'bot-id'
+        ];
+        $botResponse = new Response(json_encode($botResponseData));
+
+        $htmlInterface = m::mock(Curl::class);
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/people/me', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($botResponse);
+
+        $msgResponseData = [
+            'text' => 'Hi Julia',
+            'roomId' => 'room-1234567890',
+            'personId' => 'person-0987654321'
+        ];
+        $msgResponse = new Response(json_encode($msgResponseData));
+
+        $htmlInterface->shouldReceive('get')
+            ->once()
+            ->with('https://api.ciscospark.com/v1/messages/Y2lzY29zcGFyazovL3VzL01FU1NBR0UvMzIzZWUyZjAtOWFhZC0xMWU1LTg1YmYtMWRhZjhkNDJlZjlj', [], [
+                'Accept:application/json',
+                'Content-Type:application/json',
+                'Authorization:Bearer my-token'
+            ])
+            ->andReturn($msgResponse);
+
+        $driver = $this->getDriver($this->getValidTestData(), $htmlInterface);
+        $this->assertSame('person-0987654321', $driver->getMessages()[0]->getRecipient());
+    }
+
+    /** @test */
+    public function it_is_configured()
+    {
+        $request = m::mock(Request::class.'[getContent]');
+        $request->shouldReceive('getContent')->andReturn('');
+        $htmlInterface = m::mock(Curl::class);
+
+        $driver = new CiscoSparkDriver($request, [
+            'cisco-spark' => [
+                'token' => 'token'
+            ]
+        ], $htmlInterface);
+
+        $this->assertTrue($driver->isConfigured());
+
+        $driver = new CiscoSparkDriver($request, [
+            'cisco-spark' => [
+                'token' => null
+            ]
+        ], $htmlInterface);
+
+        $this->assertFalse($driver->isConfigured());
+
+        $driver = new CiscoSparkDriver($request, [], $htmlInterface);
+
+        $this->assertFalse($driver->isConfigured());
+    }
+}
